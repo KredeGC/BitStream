9a7f55d654a366c0720be35185c8da9481066df7
The Fern <krede2000@gmail.com>
2023-01-25 18:10:39 +0100

    Add half_precision quantization Remove old src code that was replaced

--- include/bitstream/bitstream.h
+++ include/bitstream/bitstream.h
@@ -2,9 +2,15 @@
 
 // Quantization
 #include "quantization/bounded_range.h"
+#include "quantization/half_precision.h"
 #include "quantization/smallest_three.h"
 
 // Stream
 #include "stream/bit_reader.h"
 #include "stream/bit_writer.h"
 #include "stream/serialize_traits.h"
+
+// Traits
+#include "traits/integral_traits.h"
+#include "traits/quantization_traits.h"
+#include "traits/string_traits.h"
