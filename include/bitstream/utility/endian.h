80b9a00686c67331d66bfff3ac4b5fd8818a34de
KredeGC <krede2000@gmail.com>
2023-01-28 16:45:07 +0100

    Fix endianness macros

--- include/bitstream/utility/endian.h
+++ include/bitstream/utility/endian.h
@@ -4,23 +4,40 @@
 
 #if __cplusplus < 202002L
 #ifndef BS_LITTLE_ENDIAN
-#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
-    defined(__BIG_ENDIAN__) || \
-    defined(__ARMEB__) || \
-    defined(__THUMBEB__) || \
-    defined(__AARCH64EB__) || \
-    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
-#define BS_LITTLE_ENDIAN false
-#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
-    defined(__LITTLE_ENDIAN__) || \
-    defined(__ARMEL__) || \
-    defined(__THUMBEL__) || \
-    defined(__AARCH64EL__) || \
-    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
-#define BS_LITTLE_ENDIAN true
-#else
-#error "I don't know what architecture this is!"
-#endif
+    // Detect with GCC 4.6's macro.
+#   if defined(__BYTE_ORDER__)
+#       if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#           define BS_LITTLE_ENDIAN true
+#       elif (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#           define BS_LITTLE_ENDIAN false
+#       else
+#           error "Unknown machine byteorder endianness detected. Need to manually define BS_LITTLE_ENDIAN."
+#       endif
+    // Detect with GLIBC's endian.h.
+#   elif defined(__GLIBC__)
+#       include <endian.h>
+#       if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#           define BS_LITTLE_ENDIAN true
+#       elif (__BYTE_ORDER == __BIG_ENDIAN)
+#           define BS_LITTLE_ENDIAN false
+#       else
+#           error "Unknown machine byteorder endianness detected. Need to manually define BS_LITTLE_ENDIAN."
+#       endif
+    // Detect with _LITTLE_ENDIAN and _BIG_ENDIAN macro.
+#   elif defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)
+#       define BS_LITTLE_ENDIAN true
+#   elif defined(_BIG_ENDIAN) && !defined(_LITTLE_ENDIAN)
+#       define BS_LITTLE_ENDIAN false
+    // Detect with architecture macros.
+#   elif defined(__sparc) || defined(__sparc__) || defined(_POWER) || defined(__powerpc__) || defined(__ppc__) || defined(__hpux) || defined(__hppa) || defined(_MIPSEB) || defined(_POWER) || defined(__s390__)
+#       define BS_LITTLE_ENDIAN false
+#   elif defined(__i386__) || defined(__alpha__) || defined(__ia64) || defined(__ia64__) || defined(_M_IX86) || defined(_M_IA64) || defined(_M_ALPHA) || defined(__amd64) || defined(__amd64__) || defined(_M_AMD64) || defined(__x86_64) || defined(__x86_64__) || defined(_M_X64) || defined(__bfin__)
+#       define BS_LITTLE_ENDIAN true
+#   elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
+#       define BS_LITTLE_ENDIAN true
+#   else
+#       error "Unknown machine byteorder endianness detected. Need to manually define BS_LITTLE_ENDIAN."
+#   endif
 #endif // BS_LITTLE_ENDIAN
 #else // __cplusplus < 202002L
 #include <bit>
